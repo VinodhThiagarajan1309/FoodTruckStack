37517d6a06a6c39f61923effd8d621845ce7caf3
Vinodh Thiagarajan <vinodh.thiagarajan@gmail.com>
2020-08-21 22:41:31 -0500

    Sample Status Message

--- src/foodTruckAccountsCrudFunction/index.js
+++ src/foodTruckAccountsCrudFunction/index.js
@@ -1,6 +1,9 @@
-exports.handler = async (event, context) => {
+exports.handler =  (event, context,callback) => {
   // Log the event argument for debugging and for use in local development.
   console.log(JSON.stringify(event, undefined, 2));
 
-  return {};
+  return {
+    statusCode: 200,
+    body: " This is through Stackery."
+  };
 };
